fc86fcde0641fc20e74ec60208a6d103d8cd3373
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-03-31 21:04:52 +0300

    test 1.7

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import searchImages from "./js/search";
 import axios from "axios";
 
 console.log("Asdas")
-console.log(searchImages("red")
-    .then(response => {
+console.log(searchImages("red").then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
         return response.json();
-    }).then(data => {
-        if (data.length === 0) {
-            // ("Sorry, there are no images matching your search query. Please try again.")
-        } else {
-            return data;
-        }
     }).catch(error => {
         console.log(error.mesage);
     }));
