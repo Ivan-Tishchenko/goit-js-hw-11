3f16234ad5658010358ab0b2b9b30898799e61a9
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-03-31 22:14:57 +0300

    test 1.12

--- src/js/search.js
+++ src/js/search.js
@@ -4,10 +4,11 @@ export default async function searchImages(text) {
     const serverAnsvear = await axios.get(`https://pixabay.com/api/?key=34918981-d8b731f885ecb543159a62dfc&q=${text.split(" ").join("+")}&image_type=photo&orientation=horizontal&safesearch=true`)
         .then(response => {
         if (!response.ok) {
-            throw new Error(response.status);
+        throw new Error(response.status);
         }
         return response.json();
-    }).catch(error => {
+    })
+    .catch(error => {
         console.log(error);
     });
 
