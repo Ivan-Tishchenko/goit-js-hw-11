68fa4919865d2b32a9135a3274cf4dda15fd4a98
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-03-31 20:57:35 +0300

    test 1.5

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ import searchImages from "./js/search";
 import axios from "axios";
 
 console.log("Asdas")
-console.log(searchImages("red"));
+console.log(searchImages("red")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    }).then(data => {
+        if (data.length === 0) {
+            // ("Sorry, there are no images matching your search query. Please try again.")
+        } else {
+            return data;
+        }
+    }).catch(error => {
+        console.log(error.mesage);
+    }););
--- src/js/search.js
+++ src/js/search.js
@@ -1,21 +1,7 @@
 import axios from "axios";
 
 export default async function searchImages(text) {
-    const serverAnsvear = await axios.get(`https://pixabay.com/api/?key=34918981-d8b731f885ecb543159a62dfc&q=${text.split(" ").join("+")}&image_type=photo&orientation=horizontal&safesearch=true`)
-    const imgJson = await serverAnsvear.then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    }).then(data => {
-        if (data.length === 0) {
-            // ("Sorry, there are no images matching your search query. Please try again.")
-        } else {
-            return data;
-        }
-    }).catch(error => {
-        console.log(error.mesage);
-    });
+    const serverAnsvear = await axios.get(`https://pixabay.com/api/?key=34918981-d8b731f885ecb543159a62dfc&q=${text.split(" ").join("+")}&image_type=photo&orientation=horizontal&safesearch=true`);
 
-    return imgJson;
+    return serverAnsvear;
 }
