2a2e324cf58f5528d183917280535001d4be9e6f
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-03-31 21:08:54 +0300

    test 1.8

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import searchImages from "./js/search";
 import axios from "axios";
 
 console.log("Asdas")
-console.log(searchImages("red").then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    }).catch(error => {
-        console.log(error.mesage);
-    }));
+console.log(searchImages("red").json());
--- src/js/search.js
+++ src/js/search.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 export default async function searchImages(text) {
-    const serverAnsvear = await axios.get(`https://pixabay.com/api/?key=34918981-d8b731f885ecb543159a62dfc&q=${text.split(" ").join("+")}&image_type=photo&orientation=horizontal&safesearch=true`);
+    const serverAnsvear = await axios.get(`https://pixabay.com/api/?key=34918981-d8b731f885ecb543159a62dfc&q=${text.split(" ").join("+")}&image_type=photo&orientation=horizontal&safesearch=true`)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    }).catch(error => {
+        console.log(error.mesage);
+    });
 
     return serverAnsvear;
 }
