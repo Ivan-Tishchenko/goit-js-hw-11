dd92749810c8e28557c5b82f7791e7f6c2c0df98
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-04-01 15:54:46 +0300

    test 1.18

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 console.log("Asdas")
-console.log(searchImages("red"));
+console.log(searchImages("red").json());
 
  async function searchImages(text) {
     const serverAnsvear = await axios.get(`https://pixabay.com/api/?key=34918981-d8b731f885ecb543159a62dfc&q=${text.split(" ").join("+")}&image_type=photo&orientation=horizontal&safesearch=true`)
