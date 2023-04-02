8abe2b540811c1d8762c1f71285ca39df788946f
Ivan-Tishchenko <114497011+Ivan-Tishchenko@users.noreply.github.com>
2023-04-02 10:15:21 +0300

    notefix and simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import Notiflix from "notiflix";
 
 const form = document.querySelector("#search-form");
 form.addEventListener("submit", serch)
@@ -15,12 +17,11 @@ let page = 1;
 async function serch(event) {
     event.preventDefault();
 
-    clear()
-
     if (value === input.value.trim()) {
         return;
     }
-
+    
+    clear()
     page = 1;
     value = input.value.trim();
 
@@ -32,7 +33,7 @@ async function serch(event) {
         const serverAnsvear = await searchImages(input.value, 1);
         console.log(serverAnsvear)
         if (serverAnsvear.total === 0) {
-            console.log("Sorry, there are no images matching your search query. Please try again.")
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
             return;
         }
 
@@ -40,7 +41,7 @@ async function serch(event) {
         buttonLodeMore.classList.remove("dosplay-none");
         return;
     } catch (error) {
-        console.log(error)
+        Notiflix.Notify.failure("error: " + error.massege)
     }
 }
 
@@ -56,8 +57,9 @@ async function build() {
     try {
         for (let i = 0; imagesToPage.hits.length > i; i += 1) {
             gallery.insertAdjacentHTML("beforeend", `<div class="photo-card">
-      <img src=${imagesToPage.hits[i].webformatURL} alt=${imagesToPage.hits[i].tags} href=${imagesToPage.hits[i].largeImageURL} loading="lazy" width="360"/>
-      <div class="info">
+            <a href=${imagesToPage.hits[i].largeImageURL}>
+        <img href=${imagesToPage.hits[i].largeImageURL} src=${imagesToPage.hits[i].webformatURL} alt=${imagesToPage.hits[i].tags}  loading="lazy" width="360"/>
+        <div class="info">
         <p class="info-item">
           <b>Likes</br>${imagesToPage.hits[i].likes}</b>
         </p>
@@ -71,12 +73,13 @@ async function build() {
           <b>Downloads</br>${imagesToPage.hits[i].downloads}</b>
         </p>
       </div>
+      </a>
     </div>`);
         };
 
         page += 1;
     } catch (error) {
-        console.log(error);
+        Notiflix.Notify.failure("error: " + error.massege)
     }
     return;
 }
@@ -85,3 +88,5 @@ function clear() {
     [...gallery.children].forEach(element => element.remove());
     console.log(gallery.children)
 }
+
+let galleryActive = new SimpleLightbox('.gallery a');
